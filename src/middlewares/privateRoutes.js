4ff433a5627b9ee9036ff2095c33be3cbc38b63d
lucas ribeiro <rds.lucas23@gmail.com>
2024-01-23 15:16:26 -0300

    fix

--- src/middlewares/privateRoutes.js
+++ src/middlewares/privateRoutes.js
@@ -1,6 +1,6 @@
 const privatesRoutes = (req, res, next) => {
 	const allowedRoles = ['usuario', 'admin', 'premium'];
-
+	console.log(req.session.rol);
 	if (!allowedRoles.includes(req.session.rol)) {
 		return res.redirect('/api/session/login');
 	}
