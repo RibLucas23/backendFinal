44c4715519bf4f7176cb1cb91b59b01ee06db383
lucas ribeiro <rds.lucas23@gmail.com>
2024-01-24 23:24:38 -0300

    middleware bug

--- src/routes/products.router.js
+++ src/routes/products.router.js
@@ -14,7 +14,7 @@ import premiumValidator from '../middlewares/premiumValidator.js';
 
 //=======================================__MONGO__=============================================
 //GET ALL
-productsRouter.get('/mongo', privatesRoutes, getAllProducts);
+productsRouter.get('/mongo', getAllProducts);
 //GENERATE FAKER PRODUCTS
 productsRouter.get('/mongo/mockingproducts', adminRoutes, mockingProducts);
 //CREATE PRODUCT
