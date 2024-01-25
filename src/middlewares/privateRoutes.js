e23b0a988bb56067905fcb03d376143a54fa4673
lucas ribeiro <rds.lucas23@gmail.com>
2024-01-24 23:13:01 -0300

    logs

--- src/middlewares/privateRoutes.js
+++ src/middlewares/privateRoutes.js
@@ -1,14 +1,16 @@
-const privatesRoutes = (req, res, next) => {
-	const allowedRoles = ['usuario', 'admin', 'premium'];
-	console.log('req.session:');
-	console.log(req.session);
-	console.log('req:');
-	console.log(req);
-	if (!allowedRoles.includes(req.session.rol)) {
-		return res.redirect('/api/session/login');
+const privatesRoutes = async (req, res, next) => {
+	try {
+		const allowedRoles = ['usuario', 'admin', 'premium'];
+
+		console.log('req.session:');
+		console.log(req.session);
+		if (!allowedRoles.includes(req.session.rol)) {
+			return res.redirect('/api/session/login');
+		}
+
+		next();
+	} catch (error) {
+		console.error('Error en el middleware privatesRoutes:', error);
+		res.status(500).json({ success: false, error: 'Error en el servidor' });
 	}
-
-	next();
 };
-
-export default privatesRoutes;
